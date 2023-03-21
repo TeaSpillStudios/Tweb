3052a91385a7c1d4f8dea106be9e3a8601534e98
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-21 22:50:12 +0000

    Add a basic HTTP response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7250").unwrap();
@@ -6,6 +9,22 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Recieved a connection.");
+        handle_request(stream);
     }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    stream
+        .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+        .expect("Failed to write to stream TCP.");
 }

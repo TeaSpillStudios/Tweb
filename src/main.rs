7127e21fa29a17cac5e3188e440c570d7d461cb7
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-21 22:45:23 +0000

    Establish a basic TCP Listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:7250").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Recieved a connection.");
+    }
 }

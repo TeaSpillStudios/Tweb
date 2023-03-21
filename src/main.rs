a5468ac141b3194b1eda80a53df984671ab93f5c
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-21 23:23:40 +0000

    Add CSS addition

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,57 @@ use std::net::{TcpListener, TcpStream};
 use std::path::Path;
 
 const LIVE_MODE: bool = true;
+const CSS: &str = "
+<style>
+    :root {
+    	font-family: 'Inter', sans-serif;
+    }
+    
+    @supports (font-variation-settings: normal) {
+    	:root {
+    		font-family: 'Inter var', sans-serif;
+    	}
+    }
+    
+    a {
+    	color: white;
+    }
+    
+    h1, h2, h3, h4, h5 {
+    	text-decoration: underline;
+    }
+    
+    h1 {
+    	text-align: center;
+    }
+    
+    #footer {
+    	text-align: center;
+    	font-size: 75%;
+    }
+    
+    body {
+    	background-color: rgb(24, 26, 27);
+    	color: rgb(225, 223, 219);
+    	margin-left: 5em;
+    	margin-right: 5em;
+    }
+    
+    #line_break {
+    	visibility: hidden;
+    	margin: 0.5em;
+    }
+    
+    footer {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 50px;
+        width: 100%;
+        overflow: hidden;
+    }
+</style>
+";
 
 #[derive(Default)]
 struct MarkdownLoader {
@@ -37,7 +88,7 @@ fn main() {
 
 fn handle_request(mut stream: TcpStream, markdown_loader: &mut MarkdownLoader) {
     let status = "HTTP/1.1 200 OK";
-    let data = markdown_loader.load();
+    let data = format!("<!DOCTYPE html>{}{}", CSS, markdown_loader.load());
     let length = data.len();
 
     let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{data}");

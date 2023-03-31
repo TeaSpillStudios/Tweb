1de2a30a3d3a350328db4d3dd39d6b3dfc0b1be9
TeaSpillStudios <teaspillstudios@proton.me>
2023-03-31 22:03:56 +0100

    Move HTML composition to a new file

---
+++ src/html_composer.rs
@@ -0,0 +1,44 @@
+use crate::markdown_loader::MarkdownLoader;
+
+fn format_html(page_name: &str, css: String, html: String) -> String {
+    format!(
+        "
+<!DOCTYPE html>
+<head>
+    <title>{}</title>
+    <meta charset='utf-8'>
+
+    {}
+</head>
+
+<body>
+{}
+</body>",
+        page_name, css, html
+    )
+}
+
+pub fn compose_html(page_name: &str, markdown_loader: &mut MarkdownLoader) -> String {
+    let page_exists = markdown_loader.validate_page(page_name);
+
+    let status = match page_exists {
+        true => "HTTP/1.1 200 OK",
+        false => "HTTP/1.1 404 PAGE_NOT_FOUND",
+    };
+
+    let data = match page_exists {
+        true => format_html(
+            &markdown_loader.get_page_name(page_name),
+            String::from(crate::CSS),
+            markdown_loader.load_page(page_name),
+        ),
+
+        false => format_html(
+            "# 404\nPage not found.",
+            String::from(crate::CSS),
+            markdown_loader.load_page(page_name),
+        ),
+    };
+
+    format!("{status}\r\nContent-Length: {}\r\n\r\n{data}", data.len())
+}
